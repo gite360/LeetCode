aabad392a38eb9942d7a89b638592c25934ce83f
CAMPUS\xuer <aston@aston.ac.uk>
2019-01-03 15:41:08 +0000

    190103 romanToInt

--- LeetCode/LeetCode.h
+++ LeetCode/LeetCode.h
@@ -13,6 +13,8 @@ public:
 	int reverse(int x);
 	//190102
 	bool isPalindrome(int x);
+	//190103
+	int romanToInt(string s);
 };
 
 
@@ -50,4 +52,18 @@ bool Solution::isPalindrome(int x) {
 		x /= 10;
 	}
 	return x == sum || x == sum / 10;
+}
+
+int Solution::romanToInt(string s) {
+	map<char, int> T = { {'I',1}, { 'V' , 5 }, { 'X' , 10 }, { 'L' , 50 }, { 'C' , 100 },{ 'D' , 500 }, { 'M' , 1000 } };
+	int sum = T[s.back()];
+
+	for (int i = s.length(); i >= 0; i++) {
+		if (T[s[i]] < T[s[i + 1]])
+			sum -= T[s[i]];
+		else
+			sum += T[s[i]];
+	}
+
+	return sum;
 }
