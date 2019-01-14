5384db4c51c896f1a8333381885ce21a06d1a552
CAMPUS\xuer <aston@aston.ac.uk>
2019-01-14 16:56:43 +0000

    190114b

--- LeetCode/LeetCode.h
+++ LeetCode/LeetCode.h
@@ -30,9 +30,88 @@ public:
 	int removeElement(vector<int>& nums, int val);
 	//190114
 	bool checkPossibility(vector<int>& nums);
+	
 
 };
 
+class MyLinkedList {
+public:
+	struct Node {
+		int val=NULL;
+		Node* next = nullptr;
+		Node(int val) :val(val), next(nullptr) {}
+	};
+
+	Node* head = new Node(NULL);
+	int size = NULL;
+	/** Initialize your data structure here. */
+	MyLinkedList() {
+		//head = new Node(NULL);
+		
+		size = 0;
+	}
+
+	/** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+	int get(int index) {
+		if (index >= size || index < 0) return -1;
+		Node* temp = head->next;
+		for (int i = 0; i < index; i++) {
+			temp = temp->next;
+		}
+		return temp->val;
+	}
+
+	/** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+	void addAtHead(int val) {
+		Node* temp = head->next;
+		head->next = new Node(val);
+		head->next->next = temp;
+		size++;
+	}
+
+	/** Append a node of value val to the last element of the linked list. */
+	void addAtTail(int val) {
+		Node* temp = head;
+		while (temp->next) temp = temp->next;
+		temp->next = new Node(val);
+		size++;
+	}
+
+	/** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+	void addAtIndex(int index, int val) {
+		if (index > size) return;
+		Node* temp = head;
+		for (int i = 0; i < index; i++) {
+			temp = temp->next;
+		}
+		Node* temp_node = temp->next;
+		temp->next  =	new Node(val);
+		temp->next->next = temp_node;
+		size++;
+	}
+
+	/** Delete the index-th node in the linked list, if the index is valid. */
+	void deleteAtIndex(int index) {
+		if (index >= size) return;
+		Node *temp = head;
+		for (int i = 0; i < index; i++) temp = temp -> next;
+		Node *temp_del = temp->next;
+		temp->next = temp_del -> next;
+		size--;
+		delete temp_del;
+		temp_del->next = nullptr;
+	}
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
 
 vector<int> Solution::twoSum(vector<int>& nums, int target) {
 	map <int, pair<int, int>> mp;
